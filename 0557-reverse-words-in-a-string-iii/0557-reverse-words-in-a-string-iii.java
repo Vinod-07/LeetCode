8863ba40061088b2c8d2c9e1b580237d33bea17d
Vinod Reddy <vinodreddy4103@gmail.com>
2024-06-26 01:11:49 +0530

    Time: 4 ms (88.13%), Space: 44.6 MB (97.41%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,27 +1,24 @@
 class Solution {
-    public String reverseWords(String s) {
-        String[] str = s.split(" ");
-        String ch = " ";
-        for(int i = 0; i < str.length; i++){
-            ch+= strRev(str[i]);
-            if(i < str.length-1){
-                ch+=" ";
-            }
+       public static String rev(String s){
+        int i=0,j=s.length()-1;
+        char[] c=s.toCharArray();
+        while(i<j){
+            char ch=c[i];
+            c[i]=c[j];
+            c[j]=ch;
+            i++; j--;
         }
-        return ch.trim();
+        return new String(c);
     }
-    public String strRev(String ss){
-        char[] a = ss.toCharArray();
-        int st = 0;
-            int e = a.length-1;
-            while(st < e){
-                char temp = a[st];
-                a[st] = a[e];
-                a[e]=temp;
-                st++;
-                e--;
-            }
-            String s1 = new String(a);
-            return s1;
+        
+
+    public static String reverseWords(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(String st:s.split(" ")){
+            sb.append(rev(st)).append(" ");
+
+        }
+        return sb.toString().trim();
     }
+    
 }
