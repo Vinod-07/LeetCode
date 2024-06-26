f1f657ad9b3edd430009466190774c6e3fe530c3
Vinod Reddy <vinodreddy4103@gmail.com>
2024-06-26 22:33:38 +0530

    Time: 0 ms (100%), Space: 44.5 MB (48.95%) - LeetHub

---
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int[] temp = new int[arr.length];
+
+        for(int i = 0, j = 0; j < temp.length; i++){
+            temp[j] = arr[i];
+            if(temp[j++] == 0 && j<temp.length){
+                temp[j++] = 0;
+            }
+        }
+        System.arraycopy(temp, 0, arr, 0, arr.length);
+    }
+}
