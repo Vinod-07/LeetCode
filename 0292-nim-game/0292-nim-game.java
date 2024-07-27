7cdee779fdda568c95b84a28ccab588f88e6f1a6
Vinod Reddy <vinodreddy4103@gmail.com>
2024-07-28 00:08:13 +0530

    Time: 0 ms (100%), Space: 40.2 MB (55.69%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean canWinNim(int n) {
-        if(n<=2){
-            return true;
-        }
-        if(n%4 == 0){
-            return false;
-        }
-        else{
-            return true;
-        }
+        return n%4!=0;
         
     }
 }
