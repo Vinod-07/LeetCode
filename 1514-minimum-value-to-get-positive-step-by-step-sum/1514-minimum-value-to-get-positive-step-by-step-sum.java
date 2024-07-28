f11751855a349423a34da55da829647f3011f589
Vinod Reddy <vinodreddy4103@gmail.com>
2024-07-28 21:39:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,10 +1,12 @@
 class Solution {
     public int minStartValue(int[] nums) {
         int[] prefix = new int[nums.length+1];
+        int min = 101;
         for(int i = 1; i < prefix.length; i++){
             prefix[i] = prefix[i-1] + nums[i - 1];
+            min = Math.min(prefix[i] , min);
         }
-        Arrays.sort(prefix);
-        return (Math.abs(prefix[0]) + 1);
+        
+        return (Math.abs(min) + 1);
     }
 }
