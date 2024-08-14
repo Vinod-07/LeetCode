336edc6d8c93e25eb3716c919fe4cb20dff2eed1
Vinod Reddy <vinodreddy4103@gmail.com>
2024-08-14 23:14:18 +0530

    Time: 1 ms (34.97%), Space: 40.7 MB (99.7%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -1,25 +1,44 @@
 class Solution {
     public boolean checkRecord(String s) {
-        int ca = 0;
-        int cl = 0;
-        for (char ch : s.toCharArray()) {
-            char a = ch;
-            if (a == 'A') {
-                ca++;
-            } 
-             if (a == 'L') {
-                cl++;
-                if (cl >= 3) {
-                    return false;
-                }
-            }
-            if(a == 'A' || a == 'P') {
-                cl = 0;
+        // int ca = 0;
+        // int cl = 0;
+        // for (char ch : s.toCharArray()) {
+        //     char a = ch;
+        //     if (a == 'A') {
+        //         ca++;
+        //     } 
+        //      if (a == 'L') {
+        //         cl++;
+        //         if (cl >= 3) {
+        //             return false;
+        //         }
+        //     }
+        //     if(a == 'A' || a == 'P') {
+        //         cl = 0;
+        //     }
+        // }
+        // if( ca < 2 && cl < 3)
+        //     return true;
+        // else
+        // return false;
+
+
+        String str = "LLL";
+        if(s.contains(str)){
+            return false;
+        }
+
+        int a = 0;
+        for(int i = 0; i < s.length(); i++){
+            char x = s.charAt(i);
+            if(x == 'A'){
+                a++;
             }
         }
-        if( ca < 2 && cl < 3)
-            return true;
+        if(a >= 2){
+            return false;
+        }
         else
-        return false;
+        return true;
     }
 }
