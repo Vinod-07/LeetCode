d88879e90685edac84168ae1c1241c9333cdcaa0
Vinod Reddy <vinodreddy4103@gmail.com>
2024-08-14 23:53:23 +0530

    Time: 2 ms (96.53%), Space: 41.5 MB (63.99%) - LeetHub

--- 0822-unique-morse-code-words/0822-unique-morse-code-words.java
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.java
@@ -1,14 +1,29 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
-        String[] moss = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    String[] ans = new String[words.length];
-    HashSet<String> set = new HashSet<>();
-    for(int i = 0; i < words.length; i++){
-        for(int j = 0; j < words[i].length(); j++){
-            ans[i] = ans[i] + moss[words[i].charAt(j) - 'a'];
+        // String[] moss =
+        // {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        // String[] ans = new String[words.length];
+        // HashSet<String> set = new HashSet<>();
+        // for(int i = 0; i < words.length; i++){
+        // for(int j = 0; j < words[i].length(); j++){
+        // ans[i] = ans[i] + moss[words[i].charAt(j) - 'a'];
+        // }
+        // set.add(ans[i]);
+        // }
+        // return set.size();
+
+        String[] moss = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+                "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder morseCode = new StringBuilder();
+            for (int j = 0; j < words[i].length(); j++) {
+                morseCode.append(moss[words[i].charAt(j) - 'a']);
+            }
+            set.add(morseCode.toString());
         }
-        set.add(ans[i]);
+
+        return set.size();
     }
-    return set.size();    
-}
 }
