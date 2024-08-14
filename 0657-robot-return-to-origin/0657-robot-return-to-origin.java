5401e8d54070c898c1d09e30320e5aa150213d81
Vinod Reddy <vinodreddy4103@gmail.com>
2024-08-14 23:30:22 +0530

    Time: 4 ms (97.13%), Space: 44.4 MB (43.51%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -3,23 +3,46 @@ class Solution {
         // if(moves.length() % 2 != 0){
         //     return false;
         // }
-        int ud = 0;
-        int lr = 0;
-        for(int i = 0; i < moves.length(); i++){
-            char a = moves.charAt(i);
-            if(a == 'L'){
-                lr++;
-            }
-            else if(a == 'R'){
-                lr--;
-            }
-            else if(a == 'U'){
-                ud++;
-            }
-            else{
-                ud--;
+        // int ud = 0;
+        // int lr = 0;
+        // for(int i = 0; i < moves.length(); i++){
+        //     char a = moves.charAt(i);
+        //     if(a == 'L'){
+        //         lr++;
+        //     }
+        //     else if(a == 'R'){
+        //         lr--;
+        //     }
+        //     else if(a == 'U'){
+        //         ud++;
+        //     }
+        //     else{
+        //         ud--;
+        //     }
+        // }
+        // return (ud == 0 && lr == 0) ? true : false;
+
+
+        int UpDown = 0;
+        int LeftRight = 0;
+
+        for (char c : moves.toCharArray() )
+        {
+            switch (c){
+                case 'U':
+                    UpDown++;
+                    break;
+                case 'D':
+                    UpDown--;
+                    break;
+                case 'L':
+                    LeftRight++;
+                    break;
+                case 'R':
+                    LeftRight--;
+                    break;
             }
         }
-        return (ud == 0 && lr == 0) ? true : false;
+        return (LeftRight == 0 && UpDown == 0); 
     }
 }
