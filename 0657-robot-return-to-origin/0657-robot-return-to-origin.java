15dab9257af8f03da0e057db14f9ef8917e6f636
Vinod Reddy <vinodreddy4103@gmail.com>
2024-08-14 23:23:53 +0530

    Time: 6 ms (54.94%), Space: 44.4 MB (43.51%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        if(moves.length() % 2 != 0){
-            return false;
-        }
+        // if(moves.length() % 2 != 0){
+        //     return false;
+        // }
         int ud = 0;
         int lr = 0;
         for(int i = 0; i < moves.length(); i++){
