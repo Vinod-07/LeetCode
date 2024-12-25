9be0da50e25e05bcd34ad4a45e1bcef03f637913
Vinod Reddy <vinodreddy4103@gmail.com>
2024-12-25 12:57:11 +0530

    Time: 2 ms (70.24%), Space: 41.7 MB (42.96%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,13 @@
 class Solution {
-    public int hIndex(int[] citations) {
-        // Step 1: Sort the array in descending order
-        Arrays.sort(citations);
-        int n = citations.length;
-
-        // Step 2: Traverse the sorted array and find the H-Index
-        for (int i = 0; i < n; i++) {
-            int h = n - i; // Number of papers with at least citations[i] citations
-            if (citations[i] >= h) {
+    public int hIndex(int[] c) {
+        Arrays.sort(c);
+        int n = c.length;
+        for(int i = 0; i < c.length; i++){
+            int h = n - i;
+            if(c[i] >= h){
                 return h;
             }
         }
-
-        // If no valid H-Index is found, return 0
         return 0;
     }
 }
