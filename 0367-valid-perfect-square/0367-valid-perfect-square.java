6c12852a5421b27ba6108b9a610d9a3fd64ab6c4
Vinod Reddy <vinodreddy4103@gmail.com>
2025-02-05 10:30:00 +0530

    Time: 0 ms (100%), Space: 40.6 MB (28.97%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,18 +1,32 @@
+// class Solution {
+//     public boolean isPerfectSquare(int num) {
+//         if (num < 1) return false; // Edge case
+
+//         long left = 1, right = num;
+        
+//         while (left <= right) {
+//             long mid = left + (right - left) / 2;
+//             long square = mid * mid;
+            
+//             if (square == num) return true; // Found perfect square
+//             else if (square < num) left = mid + 1;
+//             else right = mid - 1;
+//         }
+        
+//         return false; // No perfect square found
+//     }
+// }
+
+
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if (num < 1) return false; // Edge case
-
-        long left = 1, right = num;
+        if (num < 1) return false;
         
-        while (left <= right) {
-            long mid = left + (right - left) / 2;
-            long square = mid * mid;
-            
-            if (square == num) return true; // Found perfect square
-            else if (square < num) left = mid + 1;
-            else right = mid - 1;
+        long x = num;
+        while (x * x > num) { // Continue refining x
+            x = (x + num / x) / 2; // Newton's formula
         }
         
-        return false; // No perfect square found
+        return x * x == num; // Check if it's a perfect square
     }
 }
