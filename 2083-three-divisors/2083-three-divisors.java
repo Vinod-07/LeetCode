5f46caf03f33a3b418aa0a0add831109e9928423
Vinod Reddy <vinodreddy4103@gmail.com>
2025-05-19 10:38:01 +0530

    Time: 0 ms (100%), Space: 40.6 MB (38.91%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.java
+++ 2083-three-divisors/2083-three-divisors.java
@@ -5,6 +5,9 @@ class Solution {
             if(n % i == 0){
                 count++;
             }
+            if(count > 3){
+                return false;
+            }
         }
         return count == 3;
     }
