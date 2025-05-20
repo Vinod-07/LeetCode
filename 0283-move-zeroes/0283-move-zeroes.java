8ae00effb3399c35a8584fdb5da07be04f34d95b
Vinod Reddy <vinodreddy4103@gmail.com>
2025-05-20 19:58:24 +0530

    Time: 2 ms (83%), Space: 45.8 MB (80.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,48 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // int[] arr = new int[nums.length];
-        // int j = 0;
-        // int x = 0;
-        // for(int i=0; i<arr.length; i++){
-        //     if(nums[i] != 0){
-        //         arr[j] = nums[i];
-        //         j++;
-        //     }
-        //     else{
-        //         arr[nums.length - 1-x] = 0;
-        //         x++;
-        //     }
-        // }
-        // for(int i = 0; i < nums.length; i++){
-        //     nums[i]=arr[i];
-        // }
-
-        if(nums.length == 0){
-            return;
-        }
-        int s = 0, e = 1;
-        while(e<nums.length){
-            if(nums[s] == 0 && nums[e] != 0){
-                swap(nums[s], nums[e]);
-                // int temp = nums[s];
-                // nums[s] = nums[e];
-                // nums[e] = temp;
-                s++;
-                e++;
-            }
-            else if(nums[s] == 0 && nums[e] ==0){
-                e++;
-            }
-            else{
-                s++;
-                e++;
+        int zero = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[zero];
+                nums[zero] = temp;
+                zero++;
             }
         }
-    }
-    public int swap(int a, int b){
-        int temp = a;
-        a = b;
-        b = temp;
-        return int[] {a,b};
     }
 }
